7912748f651df522b54888baf2ee322e247a3486
Medini Markande <119678908+Medini1756@users.noreply.github.com>
2023-09-16 15:57:30 +0530

    Add files via upload

---
+++ songlist.js
@@ -0,0 +1,58 @@
+let allMusic=[
+    
+    {
+        name:"Heeriye",
+        artist:"Arjit Singh and Jasleen Royal",
+        img:"heeriye",
+        src:"Heeriye"
+    },
+    {
+        name:"Obsessed",
+        artist:"Riar Saab",
+        img:"obsessed",
+        src:"Obsessed"
+    },
+    {
+        name:"Pasoori",
+        artist:"Ali Sethi&Shae Gill",
+        img:"pasoori",
+        src:"Pasoori"
+    },
+    {
+        name:"Jhoome jo pathaan",
+        artist:"Arjit Singh,Vishal Dadlani,Sukriti kakar",
+        img:"pathaan",
+        src:"Pathaan"
+    },
+    {
+        name:"Tere Vaaste",
+        artist:"Varun Jain,Amitabh bhattacharya,Sachin-Jigar",
+        img:"terevaaste",
+        src:"TereVaaste"
+    },
+    {
+        name:"Maan meri jaan",
+        artist:"King",
+        img:"Maan meri jaan",
+        src:"Maan meri jaan"
+    },
+    {
+        name:"Zihal e miskin",
+        artist:"Javed Mohsin,Shreya Ghoshal",
+        img:"Zihal e miskin",
+        src:"Zihal e miskin"
+    },
+    {
+        name:"What jhumka",
+        artist:"Pritam,Arjit Singh,Jonita Gandhi",
+        img:"What Jhumka",
+        src:"What Jhumka"
+    },
+    {
+        name:"Mahiye jinna sohna",
+        artist:"Arjit Singh",
+        img:"king",
+        src:"Mahiya jinna sona"
+    },
+
+];
